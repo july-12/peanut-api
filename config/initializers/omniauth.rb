16f1446817ed40111b33d710cb5976fb3be1bb1b
july-12 <seventanyb@gmail.com>
2023-04-16 00:08:23 +0800

    fix: omi

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], provider_ignores_state: true
-  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], callback_path: "/peanut/auth/github/callback", provider_ignores_state: true
+  # provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], callback_path: "/peanut/auth/github/callback", provider_ignores_state: true
 end
